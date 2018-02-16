d7db3ba48d5d4a9c8533297a4e2e8c2ab3f027d5
Troy Lawrence <redirect@gmail.com>
2018-02-16 00:51:48 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,43 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let body = document.querySelector('body');
+let index = 0;
 
-function init() {
-  body.addEventListener('keydown', function())
+/*function init(e) {
+  body.addEventListener('keydown', function(){
+    const key = parseInt(e.detail || e.which);
+    
+    if(key === code[index]){
+     index++;
+    
+      if (index === code.length) {
+         alert('Congratulations!!!');
+         
+         index = 0;
+      }
+    } else {
+      index = 0;
+    }
+    
+  })
+}
+
+init(e);*/
+
+body.addEventListener('keydown', function init(e) {
+   const key = parseInt(e.detail || e.which);
+    
+    if(key === code[index]){
+     index++;
+    
+      if (index === code.length) {
+         alert('Congratulations!!!');
+         
+         index = 0;
+      }
+    } else {
+      index = 0;
+    }
+    
+  })
 }
