08f633eeee02a8f86b5b308eefb01e74f14de832
Troy Lawrence <redirect@gmail.com>
2018-02-16 00:52:37 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,7 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let body = document.querySelector('body');
-let index = 0;
+let index1 = 0;
 
 /*function init(e) {
   body.addEventListener('keydown', function(){
@@ -22,7 +22,7 @@ let index = 0;
   })
 }
 
-init(e);*/
+init(e);
 
 body.addEventListener('keydown', function init(e) {
    const key = parseInt(e.detail || e.which);
@@ -40,4 +40,26 @@ body.addEventListener('keydown', function init(e) {
     }
     
   })
+}*/
+
+const alphabet = [65, 66, 67];
+ 
+// Keep track of index outside of the event handler.
+let index = 0;
+ 
+// This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
 }
