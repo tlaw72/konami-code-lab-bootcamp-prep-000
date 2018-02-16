8df0853d61ecf5e7036512d52f128f327b631a83
Troy Lawrence <redirect@gmail.com>
2018-02-16 01:26:05 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ let index = 0;
 
 function init() {
   body.addEventListener('keydown', function(e){
-    const key = parseInt(e.which || e.detail);
+    const key = parseInt(e.location || e.detail);
     
     if(key === code[index]){
      index++;
