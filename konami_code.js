29944879d3f842da07ed4113dbf7fc51f22d9cd8
Troy Lawrence <redirect@gmail.com>
2018-02-16 01:14:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,8 +3,8 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let body = document.querySelector('body');
 let index = 0;
 
-function init(e) {
-  body.addEventListener('keydown', function(){
+function init() {
+  body.addEventListener('keydown', function(e){
     const key = parseInt(e.detail || e.which);
     
     if(key === code[index]){
@@ -21,4 +21,6 @@ function init(e) {
     
   })
 }
+
+init();
 
