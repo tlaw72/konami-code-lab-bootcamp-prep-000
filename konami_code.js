a0ccb3ade8d79a5709d7ac9b2ba7629d6f536343
Troy Lawrence <redirect@gmail.com>
2018-02-16 00:57:17 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -48,18 +48,3 @@ const alphabet = [65, 66, 67];
 let index = 0;
  
 // This is the function that would be invoked by the event listener.
-function onKeyDownHandler(e) {
-  const key = parseInt(e.detail || e.which);
- 
-  if (key === alphabet[index]) {
-    index++;
- 
-    if (index === alphabet.length) {
-      alert("Hurray!");
- 
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
-}
